4da71af3a0a9b68766f974203e4d480b44260f6e
Shahinur <shahinur@vonome.com>
2023-11-15 10:48:14 +0600

    docker

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -9,21 +9,12 @@ const router = express.Router();
 
 router.post(
   "/",
-
+  authMiddleware,
+  isAdmin,
   uploadPhoto.array("images", 10),
   productImageResize,
   uploadImages
 );
-
-router.delete("/delete-img/:id",  deleteImages);
-// router.post(
-//   "/",
-//   authMiddleware,
-//   isAdmin,
-//   uploadPhoto.array("images", 10),
-//   productImageResize,
-//   uploadImages
-// );
-// router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
+router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
 
 module.exports = router;
